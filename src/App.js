379141cae721ff030e1111377e13b0866eda2932
Harv <85876365+harvinder52@users.noreply.github.com>
2023-04-26 19:56:53 +0530

    remove unwanted code

--- src/App.js
+++ src/App.js
@@ -4,28 +4,10 @@ import BinaryTreeDisplay from "./BinaryTreeDisplay";
 import "./styles.css";
 
 export default function App() {
-  const [highlightedNodes, setHighlightedNodes] = useState([]);
-
-  const handleNodeClick = (nodeIndex) => {
-    const nodes = [];
-    let index = nodeIndex;
-
-    // add all nodes from the clicked node up to the root
-    while (index >= 0) {
-      nodes.push(index);
-      index = Math.floor((index - 1) / 2);
-    }
-
-    setHighlightedNodes(nodes);
-  };
-
+ 
   return (
     <div className="App">
-      <BinaryTreeDisplay
-        
-        onNodeClick={handleNodeClick}
-        highlightedNodes={highlightedNodes}
-      />
+      <BinaryTreeDisplay/>
     </div>
   );
 }
